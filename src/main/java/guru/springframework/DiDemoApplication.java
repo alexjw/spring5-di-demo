8aca1ba373cbda164b3cee1700368e8f2b20efb8
Alex J <abel.oalex@gmail.com>
2019-10-31 16:50:37 -0300

    Added Injection form Spring

--- src/main/java/guru/springframework/DiDemoApplication.java
+++ src/main/java/guru/springframework/DiDemoApplication.java
@@ -1,6 +1,9 @@
 package guru.springframework;
 
+import guru.springframework.controllers.ConstructorInjectedController;
 import guru.springframework.controllers.MyController;
+import guru.springframework.controllers.PropertyInjectedController;
+import guru.springframework.controllers.SetterInjectedController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -14,5 +17,9 @@ public class DiDemoApplication {
 		MyController controller = (MyController) run.getBean("myController");
 
 		controller.hello();
+
+		System.out.println(run.getBean(PropertyInjectedController.class).sayHello());
+		System.out.println(run.getBean(SetterInjectedController.class).sayHello());
+		System.out.println(run.getBean(ConstructorInjectedController.class).sayHello());
 	}
 }
